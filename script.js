e2bc94b6a6cf9e96c372acc15fcf5491c1a3fee1
romchy222 <romych202@mail.ru>
2025-08-11 07:41:54 +0000

    1

--- script.js
+++ script.js
@@ -16,34 +16,21 @@ renderer.setClearColor(0xaaaaaa, 1);
 // Append the renderer canvas into <body>
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.PlaneGeometry(100, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0x228B22 });
 
-// A cube we are going to animate
-const cube = {
-  // The geometry: the shape & size of the object
-  geometry: new THREE.BoxGeometry(1, 1, 1),
-  // The material: the appearance (color, texture) of the object
-  material: new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-};
+const ground = new THREE.Mesh(geometry, material);
+ground.rotation.x = -Math.PI / 2;
+ground.receiveShadow = true;
+scene.add(ground);
 
-// The mesh: the geometry and material combined, and something we can directly add into the scene (I had to put this line outside of the object literal, so that I could use the geometry and material properties)
-cube.mesh = new THREE.Mesh(cube.geometry, cube.material);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-// Add the cube into the scene
-scene.add(cube.mesh);
-
-// Make the camera further from the cube so we can see it better
-camera.position.z = 5;
-
-function render() {
-  // Render the scene and the camera
+function _rendering(){
   renderer.render(scene, camera);
-
-  // Rotate the cube every frame
-  cube.mesh.rotation.x += 0.05;
-  cube.mesh.rotation.y -= 0.05;
-
-  // Make it call the render() function about every 1/60 second
-  requestAnimationFrame(render);
+  requestAnimationFrame(_rendering);
 }
 
-render();
+_rendering();
+
